b75c31a9d98e31cef389553f6306a470e40c7d76
josieyao <yao.josephine@yahoo.com>
2019-10-17 12:19:20 -0500

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Instructor.destroy_all
+Student.destroy_all
+
+i1 = Instructor.create(name: "Vidhi")
+i2 = Instructor.create(name: "Josh")
+
+s1 = Student.create(name: "Josie", major: "Computer Science", age: 21, instructor: i2 )
+s2 = Student.create(name: "Raul", major: "Computer Science", age: 18, instructor: i1 )
